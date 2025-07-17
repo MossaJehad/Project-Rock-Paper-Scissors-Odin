7fef5936b4a3370ef86b0b390caabd82613f528a
MossaJehad <Moosa.jehad65@gmail.com>
2025-07-17 14:49:40 +0300

    : Easy task

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function startGame(choose)
+{
+	removeElem();
+	getComputerChoice = parseInt(Math.random() * 3);
+	getHumanChoice = choose;
+	let array = ['Rock', 'Paper', 'Scissor'];
+	let p = document.createElement('p');
+	let form = document.querySelector('.form');
+	p.textContent = "";
+	if (array[getComputerChoice] == 'Rock' && getHumanChoice == 'Paper'
+			|| array[getComputerChoice] == 'Paper' && getHumanChoice == 'Scissor'
+			|| array[getComputerChoice] == 'Scissor' && getHumanChoice == 'Rock')
+		p.textContent = "You win :)";
+	else if(array[getComputerChoice] == 'Paper' && getHumanChoice == 'Rock'
+			|| array[getComputerChoice] == 'Scissor' && getHumanChoice == 'Paper'
+			|| array[getComputerChoice] == 'Rock' && getHumanChoice == 'Scissor')
+		p.textContent = "You lose :(";
+	else
+		p.textContent = "draw";
+	form.append(p);
+}
+
+function removeElem()
+{
+	if (document.querySelector('p'))
+		document.querySelector('p').remove();
+}
